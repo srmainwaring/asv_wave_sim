4e101fc381fb31e795431e16d253483847c7b47e
Rhys Mainwaring <rhys.mainwaring@me.com>
2020-07-03 12:41:51 +0100

    Update wave_sim_config server.cpp
    1. Implement initial c++ version of config server.

--- wave_sim_config/src/server.cpp
+++ wave_sim_config/src/server.cpp
@@ -14,16 +14,38 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #include "wave_sim_config/server.h"
+#include "wave_sim_config/WaveSimConfig.h"
+
+#include <dynamic_reconfigure/server.h>
+#include <ros/ros.h>
+
+#include <cstdint>
 
 namespace wave_sim_config
 {
-
-
 } // wave_sim_config
 
-int main(int argc, const char **argv)
+void callback(wave_sim_config::WaveSimConfig &config, uint32_t level)
 {
+  ROS_INFO("wind_angle: %f", config.wind_angle);
+  ROS_INFO("wind_speed: %f", config.wind_speed);
+  ROS_INFO("tile_size: %f", config.tile_size);
+  ROS_INFO("tile_resolution: %d", config.tile_resolution);
+}
 
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "wave_sim_config_server");
+
+  dynamic_reconfigure::Server<wave_sim_config::WaveSimConfig> server;
+  dynamic_reconfigure::Server<wave_sim_config::WaveSimConfig>::CallbackType f;
+
+  f = boost::bind(&callback, _1, _2);
+
+  server.setCallback(f);
+
+  ROS_INFO("Start spinning");
+  ros::spin();
 
   return 0;
 }
