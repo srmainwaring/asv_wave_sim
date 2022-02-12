cf33788e45f24b6ac632c7ec5fc510e21e439412
Rhys Mainwaring <rhys.mainwaring@me.com>
2022-02-12 13:53:30 +0000

    [Ignition] Update licence notices
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

--- asv_wave_sim_gazebo_plugins/src/systems/dynamic/DynamicGeometry.cc
+++ asv_wave_sim_gazebo_plugins/src/systems/dynamic/DynamicGeometry.cc
@@ -1,4 +1,4 @@
-// Copyright (C) 2021  Rhys Mainwaring
+// Copyright (C) 2022  Rhys Mainwaring
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
