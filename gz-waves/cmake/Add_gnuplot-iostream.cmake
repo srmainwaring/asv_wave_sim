b0ad1d22de93b1c7711922f30447c74b9e5691d5
Rhys Mainwaring <rhys.mainwaring@me.com>
2023-01-17 10:15:09 +0000

    Build: update settings for gnuplot-iostream
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

--- gz-waves/cmake/Add_gnuplot-iostream.cmake
+++ gz-waves/cmake/Add_gnuplot-iostream.cmake
@@ -10,3 +10,7 @@ FetchContent_Declare(
 )
 
 FetchContent_MakeAvailable(gnuplot-iostream)
+
+# The project does not set gnuplot-iostream_INCLUDE_DIRS so use
+# gnuplot-iostream_SOURCE_DIR instead.
+set(gnuplot-iostream_INCLUDE_DIRS ${gnuplot-iostream_SOURCE_DIR})
